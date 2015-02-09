5b3866ba757658114755319d3dc55fce6b95ac97
Crispin226 <benlanier24@gmail.com>
2015-02-08 23:55:50 -0500

    Update

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,67 @@
+public class Recursion {
+public String name(){
+    return "Lanier,Benjamin";
+
+}
+
+public static void main(String[] args){
+    Recursion rec = new Recursion();
+    System.out.println(rec.fact(3));
+}
+
+public int fact(int n){
+    if (n < 0){
+	throw new IllegalArguementException("Input must be greater than 0");
+
+
+    }else{
+	return n * fact(n-1);
+    }
+}
+
+public int fib(int n){
+
+ if (n < 0){
+	throw new IllegalArguementException("Input must be greater than 0");
+
+ } else if (n == 0){
+     return 0;
+ }
+     else if (n == 1){
+	 return 1;
+     }
+     else 
+	 return fib(n-1) + fib(n-2)
+
+
+	     }
+
+public double sqrt(double n){
+
+     if (n < 0){
+	throw new IllegalArguementException("Input must be greater than 0");
+
+
+     } else if (n == 0){
+
+	 return 0;
+     } else{
+	 return sqrthelper(n, n);
+}
+}
+
+public double sqrthelper(double n, double guess){
+    double a = (n/guess + guess)/ 2.0;
+    if (Math.abs(a - guess) < 0.001){
+	return a;
+}else {
+    return sqrthelper(n, a);
+}
+}
+
+
+
+
+
+
+
