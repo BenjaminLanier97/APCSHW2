6680d4fad38e0d9b3f37cdd2313fc671057e4b93
Benjamin Lanier <benlanier24@gmail.com>
2015-05-12 12:25:11 -0400

    U

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,35 @@
+public MyHeap{
+    private int size;
+    private heap
+	private int isMax;
+ MyHeap() -> creates a max heap
+public MyHeap(boolean isMax) -> creates a max-heap when boolean is true, min-heap when the boolean is false
+
+public String toString()
+
+    public int peak(){
+ 
+ }
+public int remove() ->
+
+
+ remove the root and return the value  O(logn)
+
+public void add(int) -> add the int to the heap  O(logn)
+     if isMax
+
+public int peek() -> return the value of the root but do not remove it.  O(1)
+
+ }
+public int remove() ->
+
+
+ remove the root and return the value  O(logn)
+
+public void add(int) -> add the int to the heap  O(logn)
+     if isMax
+
+	 public int peek() {
+// return the value of the root but do not remove it.  O(1)
+							     return heap[0];
+							     }
