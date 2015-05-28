01c03e42021076e859323ca04953fe2102c8393c
Crispin226 <benlanier24@gmail.com>
2015-05-28 11:17:28 -0500

    U

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -3,36 +3,51 @@ import java.util.*;
 public class MyHeap{
    	private int[] values;
     private int size;
-    private heap
-	private int isMax;
- MyHeap() -> creates a max heap
-public MyHeap(boolean isMax) -> creates a max-heap when boolean is true, min-heap when the boolean is false
-
-public String toString()
-
-    public int peak(){
+	private boolean isMax;
+    
  
- }
+     
+     public MyHeap(boolean isMax1){
+         values = new int[50];
+         size = 50;
+         isMax = isMax1;   
+    	 }
+         
+	 	MyHeap() {
+			MyHeap(true);        
+        
+	    }
+    public String toString(){
+        
+        
+    }
+
 public int remove() ->
 
 
  remove the root and return the value  O(logn)
 
 public void add(int) -> add the int to the heap  O(logn)
-     if isMax
+    if (isMax){
+        
+    } else {
+        
+        
+    }
+}
 
-public int peek() -> return the value of the root but do not remove it.  O(1)
 
- }
-public int remove() ->
-
-
- remove the root and return the value  O(logn)
 
 public void add(int) -> add the int to the heap  O(logn)
-     if isMax
+    if (isMax){
+        
+    } else {
+        
+        
+    }
+}
 
-	 public int peek() {
-// return the value of the root but do not remove it.  O(1)
-							     return heap[0];
-							     }
+   public int peek(){
+         return heap[0];
+         }	
+}
