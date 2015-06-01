956a11f136a12cfdbc75f4319defe9e7c8c89867
Crispin226 <benlanier24@gmail.com>
2015-06-01 02:02:16 -0500

    U

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -18,11 +18,69 @@ public class MyHeap{
         
 	    }
     public String toString(){
+        String final1 = "";
+        for (int i = 0; i < size; i++){
+            final1 += values[i] + " ";
+            
+            
+        }
+        return final1;
         
         
     }
+    
+    public int GetLeftValue(int value){
+        return value * 2;
+        
+    }
+    
+    public int GetRightValue(int value){
+        return (value * 2) +1;
+        
+    }
+    
+    public int GetParentValue(int value){
+       return value/2; 
+        
+    }
+    
+    public boolean CompareNodes(int index1, int index2){
+        
+        
+        
+    }
+    
+    public static void main(String[] args){
+        MyHeap heap1 = new MyHeap();
+        heap1.add(3);
+        heap1.add(5);
+        heap1.add(6);
+        heap1.add(10);
+        System.out.println(heap1);  
+        heap1.add(12);
+        heap1.add(1);
+        
+        
+        
+        
+        
+    }
+    
+    public void resize(){
+        values = Arrays.copyOf(values, size * 2);
+        
+    }
+        
+        
+    public String name(){
+        return "lanier.benjamin";
+    }
 
-public int remove() ->
+    public int remove() {
+        
+        
+        
+    }
 
 
  remove the root and return the value  O(logn)
@@ -48,6 +106,8 @@ public void add(int) -> add the int to the heap  O(logn)
 }
 
    public int peek(){
+       if (size == 0){
+           System.out.print("
          return heap[0];
          }	
 }
