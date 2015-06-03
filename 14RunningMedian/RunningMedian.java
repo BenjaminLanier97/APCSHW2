292f8197699025b999936b7d9803c23e4befb007
Crispin226 <benlanier24@gmail.com>
2015-06-03 11:25:37 -0500

    U

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -1,5 +1,32 @@
 public RunningMedian {
+    
+    private MyHeap smaller, larger;
+    
+    public RunningMedian(){
+        smaller = new MyHeap(true);
+        larger = new MyHeap(false);
+        
+        
+    }
 
+    public String name(){
+        return "lanier.benjamin";
+    }
+    
+    public static void main(String args[]){
+        RunningMedian RM1 = new RunningMedian();
+        
+        
+        
+        
+    }
+    
+    public void add(int value){
+        
+        
+        
+    }
+    
 
 
 }
