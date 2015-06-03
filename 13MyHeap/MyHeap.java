9d2e6a915fc95493c764448fc6a647bf348aa1a7
Crispin226 <benlanier24@gmail.com>
2015-06-02 22:59:54 -0500

    U

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -4,26 +4,29 @@ public class MyHeap{
    	private int[] values;
 	private boolean isMax;
     
- 
+        
+	 	public MyHeap() {
+			this(true);        
+        
+	    }
      
      public MyHeap(boolean isMax1){
          values = new int[50];
          values[0] = 0;
          isMax = isMax1;   
     	 }
-         
-	 	MyHeap() {
-			MyHeap(true);        
-        
-	    }
+  
+    
     public String toString(){
-        String final1 = "";
-        for (int i = 0; i < values[0]; i++){
-            final1 += values[i] + " ";
+        StringBuilder FinalReturn1 = new StringBuilder();
+        int size = values[0];
+        for (int i = 0; i <= size; i++){
+            FinalReturn1.append(" ").append(values[i]);
+
             
             
         }
-        return final1;
+        return FinalReturn1.toString();
         
         
     }
@@ -53,14 +56,29 @@ public class MyHeap{
     
     public static void main(String[] args){
         MyHeap heap1 = new MyHeap();
-        heap1.add(3);
+      
         heap1.add(5);
-        heap1.add(6);
+     
+        heap1.add(2);
         heap1.add(10);
+          heap1.add(3);
+        
+        
         System.out.println(heap1);  
         heap1.add(12);
         heap1.add(1);
+            System.out.println(heap1);
         
+        
+        
+        
+    }
+    
+    
+    public void SwapTwoValues(int index1, int index2){
+      int TemporaryStorage = values[index1];
+      values[index1] = values[index2];
+        values[index2] = TemporaryStorage;
         
         
         
@@ -70,8 +88,8 @@ public class MyHeap{
     public void resize(){
         if (values[0] == values.length -1){
         values = Arrays.copyOf(values, values[0] * 2);
-        } else if (values[0] < values.length/2){
-            values = Arrays.copyOf(values, values.length / 2)
+        } else if (values[0] < values.length/2 && values[0] > 50){
+            values = Arrays.copyOf(values, values[0] / 2);
             
         }
         
@@ -82,55 +100,76 @@ public class MyHeap{
         return "lanier.benjamin";
     }
 
+        
     public int remove() {
         
-        if (heap[0] == 0){
+        if (values[0] == 0){
             throw new NoSuchElementException();
             
         }
         int TemporaryStorage = values[1];
         values[1] = values[values[0]];
         values[0] = values[0] - 1;
-        MoveValueDown(1);    
+   
+    	while ((GetLeftValue(values[1]) <= values[0]) && (CompareNodes(values[1], GetChildofValue(values[1])))) {
+	    SwapTwoValues(values[1], GetChildofValue(values[1]));
+        }
+        
+        
         return TemporaryStorage;
         
     }
     
-    public MoveValueDown(int index){
-        
-        
-        
-        
-    }
+    
+	
 
 
-
-
-public void add(int) -> add the int to the heap  O(logn)
-    if (isMax){
-        
-    } else {
-        
-        
-    }
+	public int GetChildofValue(int value) {
+	    if ((GetRightValue(value) <= values[0]) && CompareNodes(GetLeftValue(value), GetRightValue(value))){
+		return GetRightValue(value);
 }
 
+		return GetLeftValue(value);
+	}
 
-
-public void add(int) -> add the int to the heap  O(logn)
-    if (isMax){
         
-    } else {
+      
+   
+    
+    private void MoveValueUp(int index){
+        while (GetParentValue(index) > 0){
+            int IndexofParent = GetParentValue(index);
+            
+        
+            if (!CompareNodes(IndexofParent, index)){
+                SwapTwoValues(index, IndexofParent);
+                
+            }
+                index = IndexofParent;
         
         
     }
-}
+                }
+
+
+
+
+                public void add(int newIndex){
+                 
+                    values[0]++;
+                    values[values[0]] = newIndex;
+                    MoveValueUp(values[0]);
+                    resize();
+   
+				}
+
+
 
    public int peek(){
        if (values[0] == 0){
            throw new NoSuchElementException();
        }
-         return heap[1];
+         return values[1];
          
 }
 
