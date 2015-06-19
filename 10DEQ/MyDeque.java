499c0841b7d98c16f7b8fc652ba541685bf269eb
Crispin226 <benlanier24@gmail.com>
2015-06-19 03:08:56 -0500

    U

--- 10DEQ/MyDeque.java
+++ 10DEQ/MyDeque.java
@@ -1,6 +1,6 @@
 import java.util.*
     
-public class MyDeque{
+public class MyDeque<T>{
     private int Head, Tail;
     private Object[] Objects;
     
@@ -9,6 +9,67 @@ public class MyDeque{
         
     }
     
+    public String name(){
+        return "benjamin.lanier";
+        
+    }
+    
+    public void addFirst(T value){
+        
+        
+        
+    }
+    
+    public void addLast(T value){
+        
+        
+        
+    }
+    
+    public T removeFirst(){
+        
+    }
+    
+    public T removeLast(){
+        
+    }
+    
+    public T getLast(){
+        
+    }
+    
+    public T getFirst(){
+        
+        
+    }
+    
+    public String toString(){
+        
+        
+    }
+    
+    public boolean isFull(){
+        while (int i = 0; i < Objects.length; i ++){
+            if (Objects[i] = null){
+                return false;
+                
+            }
+            else{
+                return true;
+                
+                
+            }
+            
+            
+        }
+        
+        
+    }
+    
+    public static void main(String[] args){
+        
+        
+    }
 
 
 
